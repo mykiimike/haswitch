ab660ab1eded2382082577690c291ba5e3c06b71
mykiimike <mv@verman.fr>
2016-10-24 16:19:31 +0200

    Adding default configuration file

--- index.js
+++ index.js
@@ -1,150 +1,311 @@
-const execSuper = require('child_process').execSync;
+#!/usr/bin/node
+
+const EventEmitter = require('events');
 const fs = require('fs');
+const program = require('commander');
+const execSuper = require('child_process').execSync;
+const prettyjson = require('prettyjson');
 
-var file = process.argv[2];
-var mode = process.argv[3];
-var specific = process.argv[4] ? process.argv[4] : null;
-
-try {
-	var data = JSON.parse(fs.readFileSync(file));
-} catch(e) {
-	console.log(e.message);
-	process.exit(0);
+console.pretty = function(data) {
+	console.log(prettyjson.render(data));
 }
-var segment = data.segment;
-var interface = data.interface;
-var iptablesPrefix = segment.toUpperCase();
+
+function list(val) {
+	return val.split(',');
+}
+
+var commandEmulation = false;
+
+var haswitch = function(args, resource) {
+	var self = this;
+	this.opts = args;
+	this.config = {};
+
+	commandEmulation = args.emulate == true ? true:false;
+
+	/* read config file */
+	try {
+		var st = fs.statSync(args.config);
+		this.config = JSON.parse(fs.readFileSync(args.config));
+	} catch(e) {
+		console.log('Can not found JSON configuration file at '+args.config);
+		process.exit(-1);
+	}
+	var json = this.config;
+
+	/* load resource */
+	if(resource) {
+		if(!json.resources[resource]) {
+			console.log('Can not find resource '+resource+' in '+args.config);
+			process.exit(-1);
+		}
+		this.current = json.resources[resource];
+	}
+};
 
 function exec(cmd) {
-	console.log(cmd);
-	try {
-		execSuper(cmd);
-	} catch(e) {}
-}
-
-try {
-	switch(mode) {
-		case 'start':
-			/* change network state */
-			if(!specific) {
-				exec('ifup '+interface);
-				exec('iptables -t nat -F PRE'+iptablesPrefix);
-				exec('iptables -t nat -F POST'+iptablesPrefix);
-				exec('iptables -t nat -N PRE'+iptablesPrefix);
-				exec('iptables -t nat -N POST'+iptablesPrefix);
-			}
-
-			for(var a in data.machines) {
-				var machine = data.machines[a];
-				if(specific && specific != machine.vm)
-					continue;
-
-				if(machine.public) {
-					var i = machine.public.internal;
-					var e = machine.public.external;
-
-					exec('iptables -t nat -A POST'+iptablesPrefix+' -s '+i+' -j SNAT --to-source '+e);
-					exec('iptables -t nat -A PRE'+iptablesPrefix+' -d '+e+' -j DNAT --to-destination '+i);
-				}
-			}
-			
-			if(!specific) {
-				//exec('iptables -t nat -A POSTROUTING -o eth0 -j POST'+iptablesPrefix);
-				//exec('iptables -t nat -A PREROUTING -i eth0 -j PRE'+iptablesPrefix);
-			}
-
-			/* prepare drbd */
-			if(!specific) {
-				exec('drbdadm up '+segment);
-				exec('drbdadm primary '+segment);
-				exec('vgscan --cache');
-				exec('lvscan --cache');
-				exec('vgchange -ay '+segment);
-				exec('lvchange -ay '+segment);
-			}
-			/* unmount everything */
-			for(var a in data.machines) {
-				var machine = data.machines[a];
-
-				if(specific && specific != machine.vm)
-					continue;
-
-				exec('mount /dev/'+segment+'/'+machine.vm+' /data/'+segment+'/'+machine.vm);
-			}
-
-			/* start all vm */
-			for(var a in data.machines) {
-				var machine = data.machines[a];
-
-				if(specific && specific != machine.vm)
-					continue;
-				
-				exec('lxc-start -n '+machine.vm);		
-			}
-
-			break;
-	
-		case 'stop':
-			/* stop all VM */
-			for(var a in data.machines) {
-				var machine = data.machines[a];
-
-				if(specific && specific != machine.vm)
-					continue;
-
-				exec('lxc-stop -k -n '+machine.vm);		
-			}
-
-			/* unmount everything */
-			for(var a in data.machines) {
-				var machine = data.machines[a];
-
-				if(specific && specific != machine.vm)
-					continue;
-
-				exec('umount /data/'+segment+'/'+machine.vm);
-			}
-
-			/* change drbd state */
-			if(!specific) {
-				exec('vgchange -an '+segment);
-				exec('lvchange -an '+segment);
-				exec('drbdadm secondary '+segment);
-				
-				exec('vgscan --cache');
-				exec('lvscan --cache');
-
-				/* change network state */
-				//exec('iptables -t nat -D POSTROUTING -o eth0 -j POST'+iptablesPrefix);
-				//exec('iptables -t nat -D PREROUTING -i eth0 -j PRE'+iptablesPrefix);
-			}
-
-			for(var a in data.machines) {
-				var machine = data.machines[a];
-
-				if(specific && specific != machine.vm)
-					continue;
-
-				if(machine.public) {
-					var i = machine.public.internal;
-					var e = machine.public.external;
-
-					exec('iptables -t nat -D POST'+iptablesPrefix+' -s '+i+' -j SNAT --to-source '+e);
-					exec('iptables -t nat -D PRE'+iptablesPrefix+' -d '+e+' -j DNAT --to-destination '+i);
-				}
-			}
-
-			if(!specific) {
-				exec('iptables -t nat -F PRE'+iptablesPrefix);
-				exec('iptables -t nat -F POST'+iptablesPrefix);
-				//exec('iptables -t nat -X PRE'+iptablesPrefix);
-				//exec('iptables -t nat -X POST'+iptablesPrefix);
-				exec('ifdown '+interface);
-			}
-
-			break;
+	if(commandEmulation == false) {
+		console.log('[EXEC] '+cmd);
+		try {
+			execSuper(cmd);
+		} catch(e) {}
 	}
-} catch(e) {
-	console.log(e.message);
+	else {
+		console.log('[EMULATED] '+cmd);
+	}
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Start process
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function start(resource, specific) {
+	var hs = new haswitch(program, resource);
+
+	if(!resource) {
+		for(var a in hs.config.resources)
+			start(a, null)
+		process.exit(0);
+	}
+
+	/* prepare argument */
+	var data = hs.current;
+	var segment = hs.current.segment;
+	var interface = hs.current.interface;
+	var iptablesPrefix = segment.toUpperCase();
+
+	/* change network state */
+	if(!specific) {
+		exec('ifup '+interface);
+		exec('iptables -t nat -F PRE'+iptablesPrefix);
+		exec('iptables -t nat -F POST'+iptablesPrefix);
+		exec('iptables -t nat -N PRE'+iptablesPrefix);
+		exec('iptables -t nat -N POST'+iptablesPrefix);
+	}
+
+	/* Add iptable forwarding */
+	for(var a in data.machines) {
+		var machine = data.machines[a];
+		if(specific && specific != machine.vm)
+			continue;
+
+		if(machine.public) {
+			var i = machine.public.internal;
+			var e = machine.public.external;
+
+			exec('iptables -t nat -A POST'+iptablesPrefix+' -s '+i+' -j SNAT --to-source '+e);
+			exec('iptables -t nat -A PRE'+iptablesPrefix+' -d '+e+' -j DNAT --to-destination '+i);
+		}
+	}
+
+	/* prepare drbd */
+	if(!specific && data.drbd == true) {
+		exec('drbdadm up '+segment);
+		exec('drbdadm primary '+segment);
+		exec('vgscan --cache');
+		exec('lvscan --cache');
+		exec('vgchange -ay '+segment);
+		exec('lvchange -ay '+segment);
+	}
+
+	/* unmount everything */
+	for(var a in data.machines) {
+		var machine = data.machines[a];
+
+		if(specific && specific != machine.vm)
+			continue;
+
+		exec('mount /dev/'+segment+'/'+machine.vm+' /data/'+segment+'/'+machine.vm);
+	}
+
+	/* start all vm */
+	for(var a in data.machines) {
+		var machine = data.machines[a];
+
+		if(specific && specific != machine.vm)
+			continue;
+
+		exec('lxc-start -n '+machine.vm);
+	}
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Stop process
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function stop(resource, specific) {
+	var hs = new haswitch(program, resource);
+
+	if(!resource) {
+		for(var a in hs.config.resources)
+			stop(a, null)
+		process.exit(0);
+	}
+
+	/* prepare argument */
+	var data = hs.current;
+	var segment = hs.current.segment;
+	var interface = hs.current.interface;
+	var iptablesPrefix = segment.toUpperCase();
+
+	/* stop all VM */
+	for(var a in data.machines) {
+		var machine = data.machines[a];
+
+		if(specific && specific != machine.vm)
+			continue;
+
+		exec('lxc-stop -k -n '+machine.vm);
+	}
+
+	/* unmount everything */
+	for(var a in data.machines) {
+		var machine = data.machines[a];
+
+		if(specific && specific != machine.vm)
+			continue;
+
+		exec('umount /data/'+segment+'/'+machine.vm);
+	}
+
+	/* change drbd state */
+	if(!specific && data.drbd == true) {
+		exec('vgchange -an '+segment);
+		exec('lvchange -an '+segment);
+		exec('drbdadm secondary '+segment);
+
+		exec('vgscan --cache');
+		exec('lvscan --cache');
+	}
+
+	for(var a in data.machines) {
+		var machine = data.machines[a];
+
+		if(specific && specific != machine.vm)
+			continue;
+
+		if(machine.public) {
+			var i = machine.public.internal;
+			var e = machine.public.external;
+
+			exec('iptables -t nat -D POST'+iptablesPrefix+' -s '+i+' -j SNAT --to-source '+e);
+			exec('iptables -t nat -D PRE'+iptablesPrefix+' -d '+e+' -j DNAT --to-destination '+i);
+		}
+	}
+
+	if(!specific) {
+		exec('iptables -t nat -F PRE'+iptablesPrefix);
+		exec('iptables -t nat -F POST'+iptablesPrefix);
+		exec('ifdown '+interface);
+	}
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Restart process
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function restart(resource, specific) {
+	stop(resource, specific);
+	start(resource, specific);
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Show configuration process
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function show() {
+	var hs = new haswitch(program);
+	console.pretty(hs.config);
+	process.exit(0);
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Network process
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function network(cmd, resource, subcall) {
+	var hs = new haswitch(program, resource);
+
+	if(!resource) {
+		for(var a in hs.config.resources)
+			network(cmd, a, true)
+		process.exit(0);
+	}
+
+	/* prepare argument */
+	var data = hs.current;
+	var segment = hs.current.segment;
+	var interface = hs.current.interface;
+	var iptablesPrefix = segment.toUpperCase();
+
+	if(cmd == 'init') {
+
+		exec('iptables -t nat -N PRE'+iptablesPrefix);
+		exec('iptables -t nat -N POST'+iptablesPrefix);
+
+		exec('iptables -t nat -F PRE'+iptablesPrefix);
+		exec('iptables -t nat -F POST'+iptablesPrefix);
+
+		exec('iptables -t nat -A POSTROUTING -o eth0 -j POST'+iptablesPrefix);
+		exec('iptables -t nat -A PREROUTING -i eth0 -j PRE'+iptablesPrefix);
+		if(subcall != true)
+			process.exit(0)
+		return;
+	}
+	else if(cmd == 'fini') {
+		exec('iptables -t nat -X PRE'+iptablesPrefix);
+		exec('iptables -t nat -X POST'+iptablesPrefix);
+
+		exec('iptables -t nat -D POSTROUTING -o eth0 -j POST'+iptablesPrefix);
+		exec('iptables -t nat -D PREROUTING -i eth0 -j PRE'+iptablesPrefix);
+		if(subcall != true)
+			process.exit(0)
+		return;
+	}
+
+	console.log('Command not found');
+	process.exit(-1)
+}
+
+
+program
+	.arguments('<resource>')
+	.option('-c, --config [file]', 'Configuration file', '/etc/haswitch.json')
+	.option('-e, --emulate [on/off]', 'Just print commands w/o execution', false)
+	.option('-v, --verbose');
+
+program
+  .command('start [resource] [machine]')
+  .action(start);
+
+program
+  .command('stop [resource] [machine]')
+  .action(stop);
+
+program
+  .command('restart [resource] [machine]')
+  .action(restart);
+
+program
+  .command('network <init/fini> [resource]')
+	.description('Initialize or terminate network chains')
+  .action(network);
+
+program
+  .command('show')
+	.description('Show configuration file')
+  .action(show);
+
+program.on('--help', function(){
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ haswitch start ha0        Start all machines from ha0');
+	console.log('    $ haswitch start ha0 vm1ha0 Start machines vm1ha0 from ha0');
+	console.log('    $ haswitch network init     Initialize routing chains');
+  console.log('');
+	console.log('  haswitch.js (c) 2016 - Michael Vergoz');
+});
+
+program.parse(process.argv);
